42cb37ec2a98abaae8b2d3c194f018344b2fc4ff
anshikavashistha <anshikav1534@gmail.com>
2023-10-29 03:21:17 -0700

    first commit

--- portfolio/script.js
+++ portfolio/script.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
         if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
         } else {
+        
             $('.scroll-up-btn').removeClass("show");
         }
     });
